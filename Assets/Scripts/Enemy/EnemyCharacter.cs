c4228f11895b9db4f29a9d146d834d52d73cbc81
kor1b <39777055+kor1b@users.noreply.github.com>
2019-08-03 15:42:34 +0300

    Imported Enemy AI, EnemyCharacket base

---
+++ Assets/Scripts/Enemy/EnemyCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyCharacter : MonoBehaviour
+{
+	private void OnEnable()
+	{
+		SetParameters ();
+	}
+
+	protected abstract void SetParameters();
+
+	public abstract void TakeDamage(int damage);
+
+	protected void Death()
+	{
+		//TODO: return to pool
+		Debug.Log ("Killed " + gameObject.name);
+	}
+
+	
+}
---
+++ Assets/Scripts/Enemy/EnemySoldier.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySoldier : EnemyCharacter
+{
+	private int rank = 2;               //rank of number
+
+	protected override void SetParameters()
+	{
+		rank = Random.Range (2, 9);
+	}
+
+	public override void TakeDamage(int damage)
+	{
+		if (damage >= rank)
+			Death ();
+		else
+			Heal ();
+	}
+
+	void Heal()
+	{
+		//increase rank
+		rank++;
+		Debug.Log ("Rank " + rank);
+	}
+}
