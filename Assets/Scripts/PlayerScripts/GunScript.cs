c24cebf7bc5e040f0fb0100790ca64842c8f9a91
MrMichaelUm <50107636+MrMichaelUm@users.noreply.github.com>
2019-08-03 16:07:10 +0300

    Player movement end rotations

---
+++ Assets/Scripts/PlayerScripts/GunScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public GameObject Player;
+
+    private void FixedUpdate()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+
+        float rotation = Mathf.Atan2(difference.y, difference.x)*Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation);
+
+        if (rotation < -90 || rotation > 90)
+        {
+            if (Player.transform.eulerAngles.y == 0)
+            {
+                transform.localRotation = Quaternion.Euler(180f, 0f, -rotation);
+            }
+            else if(Player.transform.eulerAngles.y == 180)
+            {
+                transform.localRotation = Quaternion.Euler(180f, 180f, -rotation);
+            }
+        }
+    }
+}
