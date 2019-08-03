b31447c1a5662f90ab92a0866624bed36a9cdeb0
tardis2snej <nadya.snezhik@gmail.com>
2019-08-03 20:58:55 +0300

    нарисован коридор и генерируется один-единственный шип в конкретной точке

---
+++ Assets/Scripts/Coridor/SpikesSpawn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SpikesSpawn : MonoBehaviour
+{
+    [SerializeField] [Tooltip("Тайл шипов")]
+    private TileBase tile;
+    private Tilemap tilemap;
+
+    void Awake()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    private void OnEnable()
+    {
+        tilemap.SetTile(new Vector3Int(-8, -4, 0), tile);
+    }
+}
